012951fe5ececb4b5c55aa051b47ed0fae98a904
ianodad <ianodad@gmail.com>
2021-04-07 12:18:09 +0300

    graphql backend for

---
+++ schema/schema.js
@@ -0,0 +1,37 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+const axios =require('axios');
+
+const {GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema} = graphql;
+
+const users = [
+    {id:'23', firstName:'John', lastName:'Doe', age:20},
+    {id:'24', firstName:'Jane', LastName:'Down', age:26}
+];
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString},
+        firstName: { type: GraphQLString},
+        lastName: { type: GraphQLString},
+        age: { type: GraphQLInt}
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name:'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString }},
+            resolve(parentValue, args){
+                return _.find(users, {id: args.id});
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query:RootQuery
+});
